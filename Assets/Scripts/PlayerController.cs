cb725fbcccaef743a414d695a23a91edd480602c
Marc-Andre Larocque <Marc-Andre Larocque>
2016-03-02 16:35:44 -0500

    Terminer le projet

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,12 +7,20 @@ public class PlayerController : MonoBehaviour {
     public float speed;
     public Text countText;
     public Text winText;
+    public Text deathText;
 
     private Rigidbody rb;
+
     private int count;
+    private int freezeTime = 5;
+
+    private bool debugMode = true;
+
+    private static int amountDeaths = 0;
 
     void Start()
     {
+        deathText.text = "Deaths: " + amountDeaths.ToString();
         count = 0;
         rb = GetComponent<Rigidbody>();
         setCountText();
@@ -26,7 +34,18 @@ public class PlayerController : MonoBehaviour {
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce(movement * speed);
+
+        if(debugMode)
+            if(Input.GetKeyDown(KeyCode.R))
+            {
+                reset();
+            }
 	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        rb.AddForce(collision.contacts[0].normal , ForceMode.Impulse);
+    }
 
     void OnTriggerEnter(Collider other)
     {
@@ -36,13 +55,52 @@ public class PlayerController : MonoBehaviour {
             count++;
             setCountText();
         }
+
+        else if(other.gameObject.CompareTag("Hole"))
+          {
+            death();
+          }
+        
+        else if (other.gameObject.CompareTag("Rebound"))
+        {
+            rb.AddForce(rb.velocity.normalized * -15 , ForceMode.Impulse);
+        }
     }
 
     void setCountText()
     {
         countText.text = "Score = " + count.ToString();
         if(count >= 12){
-            winText.text = "GG WP";
+            freezeGame(freezeTime);
         }
+    }
+    IEnumerator ResetAfterSeconds(int seconds)
+    {
+        float pauseEndTime = Time.realtimeSinceStartup + seconds;
+        while (Time.realtimeSinceStartup < pauseEndTime)
+        {
+            yield return null;  // Attend un frame      
+        }
+
+        reset();
+    }
+
+    void reset()
+    {
+        Application.LoadLevel(0);
+        Time.timeScale = 1f;
+    }
+
+    void freezeGame(int amount)
+    {
+        StartCoroutine(ResetAfterSeconds(amount));
+        Time.timeScale = 0f;
+    }
+
+    void death()
+    {
+        amountDeaths++;
+        winText.text = "YOU LOSE";
+        freezeGame(freezeTime);    
     }
 }
